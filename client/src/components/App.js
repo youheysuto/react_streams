6d18a00f85e99869f3a474658e24985c2e922ecd
須藤洋平 <ricken31@sudouyouheinoMacBook-Pro.local>
2019-05-02 19:32:58 +0900

    add: test router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            PageOne
+            <Link to="/pagetwo">Navigate to Page two</Link>
+        </div>
+    )
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <Link to="/">Navigate to Page one</Link>
+        </div>
+    )
+}
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                App
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" exact={true} component={PageOne} />
+                        <Route path="/pagetwo" component={PageTwo} />
+                    </div>
+                </BrowserRouter>
             </div>
         )
     }
