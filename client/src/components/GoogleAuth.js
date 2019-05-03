866e3f88bb5d192f78d6b0e4ebb96e02eb12210a
須藤洋平 <ricken31@sudouyouheinoMacBook-Pro.local>
2019-05-03 21:12:30 +0900

    add redux for saving auth data to reduxStore

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { connect } from "react-redux";
+
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
-
-    state = { isSignedIn : null }
-
     componentDidMount(){
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -12,14 +12,19 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(()=>{
                 this.auth = window.gapi.auth2.getAuthInstance()
-                this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+
+                this.onAuthChange(this.auth.isSignedIn.get())
                 this.auth.isSignedIn.listen(this.onAuthChange); // 認証状態が変わった際に発火
             });
         });
     }
 
-    onAuthChange = () => {
-        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn){
+            this.props.signIn(this.auth.currentUser.get().getId());
+        } else {
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -31,9 +36,9 @@ class GoogleAuth extends React.Component {
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOutClick} className="ui red google button">
                     <i className="google icon" />
@@ -57,4 +62,8 @@ class GoogleAuth extends React.Component {
     }
 }
 
-export default GoogleAuth;
+const matStateToProps = (state) => {
+    return {isSignedIn: state.auth.isSignedIn}
+}
+
+export default connect(matStateToProps, {signIn, signOut})(GoogleAuth);
