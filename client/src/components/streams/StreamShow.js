cf91253f64ced737c859301c3b448b9a50772cea
youheysuto <fugazi.utada@gmail.com>
2019-06-02 20:05:50 +0900

    flvplayerセットアップ

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -5,14 +5,21 @@ import {fetchStream} from '../../actions';
 
 class StreamShow extends React.Component{
 
+    // レンダリング後のvideoタグをflvに渡すためにcreateRefする
     constructor(props){
         super(props);
-
         this.videoRef = React.createRef();
     }
 
     componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id);
+        const {id} = this.props.match.params;
+        this.props.fetchStream(id);
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
